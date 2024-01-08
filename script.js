de64c043e0370779592a45191acff25f681b8215
jamesherriott1 <>
2024-01-08 07:40:01 +0000

    feat:added console.log to buildDeck

--- script.js
+++ script.js
@@ -10,18 +10,24 @@ let playerCardBack;
 let canTwist = true;
 // player can twist while playerSum <= 21
 
+window.onload = function(){
+    buildDeck();
+}
 
-
-document.addEventListener('DOMContentLoaded', function buildDeck() {
+function buildDeck() {
     let value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     let suit = ['C', 'D', 'H', 'S'];
+    deck = [];
 
-    for (let i = 0; i < suits.lenght; i++) {
-        for (let b = 0; b < values.lenght; b++) {
+    for (let i = 0; i < suit.lenght; i++) {
+        for (let b = 0; b < value.lenght; b++) {
             deck.push(`${value[b]}-${suit[i]}`);
         }
     }
-});
+    console.log(deck)
+}
+
+
 
 
 function shuffleDeck() {
